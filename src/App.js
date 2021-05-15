6e599f9023003121ee8a0b849b2ac5495e6f96ea
Oluwaseun Adebambo <seun.bambo@gmail.com>
2021-05-15 22:19:59 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -10,30 +10,77 @@ import Dashboard from './componets/main/home/Dashboard';
 import PrivateRoute from './componets/private/PrivateRoute';
 import Navbar from './componets/layouts/Navbar';
 import FilteredTable from './componets/main/table-elements/table/FilteredTable';
+import { getWeb3 } from './utils.js';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Router>
-          <Navbar />
-          <div className='App container'>
-            <Switch>
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute
-                exact
-                path='/vouchers/:status/:type'
-                component={FilteredTable}
-              />
-              <Route exact path='/' component={Login} />
-              <Route exact path='/sign-in' component={Login} />
-              <Route exact path='/sign-up' component={Register} />
-            </Switch>
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const networkId = await web3.eth.net.getId();
+
+      if (networkId !== 4) {
+        return (
+          <div>
+            Please install Metamask plugin and ensure you switch to Rinkeby Test
+            Network.
           </div>
-        </Router>
-      </PersistGate>
-    </Provider>
-  );
+        );
+      }
+
+      setWeb3(web3);
+      setAccounts(accounts);
+      setContract(contract);
+    };
+    init();
+    window.ethereum.on('accountsChanged', (accounts) => {
+      setAccounts(accounts);
+    });
+  });
+
+  const isReady = () => {
+    return (
+      typeof contract !== 'undefined' &&
+      typeof web3 !== 'undefined' &&
+      typeof accounts !== 'undefined'
+    );
+  };
+
+  if (!isReady()) {
+    return (
+      <div className='text-light'>
+        Please install Metamask plugin and ensure you switch to Rinkeby Test
+        Network.
+      </div>
+    );
+  } else {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Router>
+            <Navbar />
+            <div className='App container'>
+              <Switch>
+                <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                <PrivateRoute
+                  exact
+                  path='/vouchers/:status/:type'
+                  component={FilteredTable}
+                />
+                <Route exact path='/' component={Login} />
+                <Route exact path='/sign-in' component={Login} />
+                <Route exact path='/sign-up' component={Register} />
+              </Switch>
+            </div>
+          </Router>
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/componets/main/vouchers/add/AddVoucherForm.js
+++ src/componets/main/vouchers/add/AddVoucherForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import socketIOClient from 'socket.io-client';
+import Web3 from 'web3';
+// import { getWeb3 } from '../../../../utils';
 
 import { FormInput } from '../../../reusable/FormInput';
 import { Button } from '../../../reusable/Button';
@@ -7,6 +9,47 @@ import { addNewVoucher } from '../../../../services/voucher.service';
 import { apiEndPoint } from '../../../../Config';
 
 const API_ENDPOINT = apiEndPoint();
+
+const Subscription = require('../../../../contracts/Subscription.json');
+
+const RANDOM_VALUE_MULTIPLIER = 10001;
+
+const voucherGeneratedId = `${Math.floor(
+  Math.random() * RANDOM_VALUE_MULTIPLIER
+)}`;
+
+const createVoucherOnChain = async (amount, voucherId) => {
+  try {
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider('http://localhost:7545')
+    );
+    // const web3 = new Web3(
+    //   new Web3.providers.HttpProvider(`${process.env.INFURA_URL}`)
+    // );
+    const id = await web3.eth.net.getId();
+    const deployedNetwork = Subscription.networks[id];
+    const contract = new web3.eth.Contract(
+      Subscription.abi,
+      deployedNetwork.address
+    );
+    const addresses = await web3.eth.getAccounts();
+
+    // const receipt = await contract.methods.createVoucher(voucherGeneratedId, amount).send({
+    const receipt = await contract.methods
+      .createVoucher(amount, voucherId)
+      .send({
+        from: addresses[0],
+        gas: 4700000,
+        gasPrice: '2000000000',
+      });
+    const voucher = await contract.methods.getVouchers(1).call();
+
+    console.log('Receipt', receipt);
+    console.log('Vouchers', voucher);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const AddVoucherForm = (props) => {
   const socket = socketIOClient(API_ENDPOINT);
@@ -16,13 +59,16 @@ const AddVoucherForm = (props) => {
   const [voucher, setVoucher] = useState({
     data: {
       amount: '',
+      voucherId: '',
     },
   });
 
   const { amount } = voucher.data;
 
   const onAddVoucher = async (e) => {
+    createVoucherOnChain(amount, voucherGeneratedId);
     e.preventDefault();
+    voucher.data.voucherId = voucherGeneratedId;
     const { data } = voucher;
     await addNewVoucher(data);
     socket.emit('refresh', {});
